3b42075163ebf53c4a0c9f14c49aace69224fdfd
callumacrae <callum@lynxphp.com>
2014-01-24 16:14:23 +0000

    Removed RequireJS and added email address.

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,15 +1,28 @@
-if (typeof DEBUG === 'undefined') {
-	DEBUG = true;
-}
-
 /**
- * macrae
+ * macr.ae
  *
  * Copyright (c) 2014 Callum Macrae
  */
 
-require([], function () {
-	'use strict';
+(function () {
+	// Hide no-js paragraph
+	var p = document.querySelectorAll('p.no-js');
+	p = p[p.length - 1];
 
+	p.classList.remove('no-js');
 
-});
+	// Add email address. Is this over the top?
+	var emailElement = document.querySelectorAll('.add-email');
+	emailElement = emailElement[emailElement.length - 1];
+
+	if ('ontouchstart' in window || navigator.msMaxTouchPoints) {
+		addAddr();
+	}
+
+	emailElement.addEventListener('mouseover', addAddr);
+
+	function addAddr() {
+		var a = 'http://callum\u0040ma\u0063r\u002Eae';
+		this.href = a.replace('http://', 'ma\u0069' + 'lto:');
+	}
+})();
