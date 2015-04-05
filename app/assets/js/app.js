6b70b13670dcac6c767da2e20caec86f66688171
Callum Macrae <callum@macr.ae>
2015-04-05 16:58:20 +0100

    Deleted some javascript

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -7,12 +7,6 @@
 (function () {
 	'use strict';
 
-	// Hide no-js paragraph
-	var p = document.querySelectorAll('p.no-js');
-	p = p[p.length - 1];
-
-	p.classList.remove('no-js');
-
 	// Add email address. Is this over the top?
 	var emailElement = document.querySelectorAll('.add-email');
 	emailElement = emailElement[emailElement.length - 1];
