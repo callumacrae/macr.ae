252b4684f2f4409ca83add9c766a4b604f794fcb
Callum Macrae <callum@macr.ae>
2015-11-10 19:33:28 +0000

    lazy load social media

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 require('prismjs');
-
+var whenScroll = require('when-scroll');
 var isTouch = require('touch-screen');
 
 // Add email address. Is this over the top?
@@ -23,3 +23,20 @@ if (isTouch()) {
 
 document.documentElement.classList.add('js');
 document.documentElement.classList.remove('no-js');
+
+var social = document.querySelector('.social');
+whenScroll(['within 500px of', social], function () {
+	loadScript('https://connect.facebook.net/en_GB/all.js#xfbml=1&appId=251120905643');
+	window.___gcfg = {lang: 'en-GB'};
+	loadScript('https://apis.google.com/js/plusone.js');
+	loadScript('https://platform.twitter.com/widgets.js');
+});
+
+function loadScript(url) {
+	var first, js, d = document, s = 'script';
+
+	first = d.getElementsByTagName(s)[0];
+	js = d.createElement(s);
+	js.src = url;
+	first.parentNode.insertBefore(js, first);
+}
