af42ec76b9bc70d7b9b97a008f998c7f2ca51626
Callum Macrae <callum@samknows.com>
2017-02-22 10:49:28 +0000

    Added bio and article descriptions to index page

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -5,20 +5,7 @@ require('./hr');
 var whenScroll = require('when-scroll');
 var isTouch = require('touch-screen');
 
-// Add email address. Is this over the top?
-var emailElement = document.querySelectorAll('.add-email');
-emailElement = emailElement[emailElement.length - 1];
-
-emailElement.addEventListener('mouseover', addAddr);
-
-function addAddr() {
-	var a = 'http://callum\u0040ma\u0063r\u002Eae';
-	emailElement.href = a.replace('http://', 'ma\u0069' + 'lto:');
-}
-
 if (isTouch()) {
-	addAddr();
-
 	document.documentElement.classList.add('touch');
 }
 
