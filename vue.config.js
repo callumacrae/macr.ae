adc97cd1389b8ed5f85ba6d435d909b84888153a
Callum Macrae <callum@samknows.com>
2019-11-25 22:42:44 +0000

    Don't try to build if build won't work

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
+const chromium = require('chrome-aws-lambda');
 const Renderer = require('./webpack/zeit-renderer');
 
 const articles = fs
   .readdirSync('./articles/')
   .map(file => `/article/${file.slice(0, -3)}`);
 
+const plugins = [];
+
+if (chromium.headless) {
+  plugins.push(
+    new PrerenderSPAPlugin({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/', ...articles],
+      renderer: new Renderer()
+    })
+  );
+}
+
 module.exports = {
   configureWebpack: {
-    plugins: [
-      new PrerenderSPAPlugin({
-        staticDir: path.join(__dirname, 'dist'),
-        routes: ['/', ...articles],
-        renderer: new Renderer()
-      })
-    ]
+    plugins
   }
 };
